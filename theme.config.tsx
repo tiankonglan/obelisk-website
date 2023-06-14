3b4b638a7d38e4c9631596d0b87ad40205dfaa25
Davidxing1 <937104001@qq.com>
2023-06-14 17:03:22 +0800

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -93,55 +93,55 @@ const config: DocsThemeConfig = {
 
     return (
       <>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`/images/favicon-${systemTheme}/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`/images/favicon-${systemTheme}/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`/images/favicon-${systemTheme}/favicon-16x16.png`}
-        />
-        <link
-          rel="mask-icon"
-          href={`/images/favicon-${systemTheme}/safari-pinned-tab.svg`}
-          color="#000000"
-        />
-        <link
-          rel="shortcut icon"
-          href={`/images/favicon-${systemTheme}/favicon.ico`}
-        />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="theme-color" content="#000" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@turborepo" />
-        <meta name="twitter:creator" content="@turborepo" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={fullUrl} />
-        <link rel="canonical" href={fullUrl} />
-        <meta property="twitter:image" content={ogUrl} />
-        <meta property="og:image" content={ogUrl} />
-        <meta property="og:locale" content="en_IE" />
-        <meta property="og:site_name" content="Obelist" />
-        <link rel="prefetch" href="/repo" as="document" />
-        <link rel="prefetch" href="/repo/docs" as="document" />
-        <link rel="prefetch" href="/pack" as="document" />
-        <link rel="prefetch" href="/pack/docs" as="document" />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="Obelisk Blog"
-          href="https://turbo.build/feed.xml"
-        />
+        {/*<meta name="viewport" content="width=device-width, initial-scale=1.0" />*/}
+        {/*<link*/}
+        {/*  rel="apple-touch-icon"*/}
+        {/*  sizes="180x180"*/}
+        {/*  href={`/images/favicon-${systemTheme}/apple-touch-icon.png`}*/}
+        {/*/>*/}
+        {/*<link*/}
+        {/*  rel="icon"*/}
+        {/*  type="image/png"*/}
+        {/*  sizes="32x32"*/}
+        {/*  href={`/images/favicon-${systemTheme}/favicon-32x32.png`}*/}
+        {/*/>*/}
+        {/*<link*/}
+        {/*  rel="icon"*/}
+        {/*  type="image/png"*/}
+        {/*  sizes="16x16"*/}
+        {/*  href={`/images/favicon-${systemTheme}/favicon-16x16.png`}*/}
+        {/*/>*/}
+        {/*<link*/}
+        {/*  rel="mask-icon"*/}
+        {/*  href={`/images/favicon-${systemTheme}/safari-pinned-tab.svg`}*/}
+        {/*  color="#000000"*/}
+        {/*/>*/}
+        {/*<link*/}
+        {/*  rel="shortcut icon"*/}
+        {/*  href={`/images/favicon-${systemTheme}/favicon.ico`}*/}
+        {/*/>*/}
+        {/*<meta name="msapplication-TileColor" content="#000000" />*/}
+        {/*<meta name="theme-color" content="#000" />*/}
+        {/*<meta name="twitter:card" content="summary_large_image" />*/}
+        {/*<meta name="twitter:site" content="@turborepo" />*/}
+        {/*<meta name="twitter:creator" content="@turborepo" />*/}
+        {/*<meta property="og:type" content="website" />*/}
+        {/*<meta property="og:url" content={fullUrl} />*/}
+        {/*<link rel="canonical" href={fullUrl} />*/}
+        {/*<meta property="twitter:image" content={ogUrl} />*/}
+        {/*<meta property="og:image" content={ogUrl} />*/}
+        {/*<meta property="og:locale" content="en_IE" />*/}
+        {/*<meta property="og:site_name" content="Obelist" />*/}
+        {/*<link rel="prefetch" href="/repo" as="document" />*/}
+        {/*<link rel="prefetch" href="/repo/docs" as="document" />*/}
+        {/*<link rel="prefetch" href="/pack" as="document" />*/}
+        {/*<link rel="prefetch" href="/pack/docs" as="document" />*/}
+        {/*<link*/}
+        {/*  rel="alternate"*/}
+        {/*  type="application/rss+xml"*/}
+        {/*  title="Obelisk Blog"*/}
+        {/*  href="https://turbo.build/feed.xml"*/}
+        {/*/>*/}
       </>
     );
   },
